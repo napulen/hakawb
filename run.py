53b6c39522df9aae9ddf9063ddbf43ea8f5987a9
Nestor Napoles <napulen@gmail.com>
2019-03-02 16:49:00 -0500

    New: Combining all pc-sets from major and minor chords

--- run.py
+++ run.py
@@ -12,26 +12,46 @@ def initLogger():
 major_keys = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B']
 minor_keys = [k.lower() for k in major_keys]
 
-major_pc_sets = {}
+def get_pc_sets(base, keys):
+    pc_sets = {}
+    base_harm = base.keys()
+    inversions = ['', 'b', 'c', 'd']
+    for key in keys:
+        for inversion in inversions:
+            for harm in base_harm:
+                harmstr = '{}:{}{}'.format(key, harm, inversion)
+                if inversion == 'd':
+                    if harm != 'Gn' and harm != 'Fr' and not '7' in harm:
+                        continue
+                c = chord.Chord(harmstr)
+                logger.info('{} | {} {} | {} | inversion {} | bass {} | {}'.format(c.harmstr, c.key, 'minor' if c.key.islower() else 'major', c.harm, c.inversion, c.bass, c.pitch_classes))
+                pc_set = pc_sets.get(c.pitch_classes, [])
+                pc_set.append(c.harmstr)
+                pc_sets[c.pitch_classes] = pc_set
+    return pc_sets
+
+
+def add_pc_sets(a, b):
+    c = {}
+    c_keys = list(a.keys()) + list(b.keys())
+    for pc_set in set(c_keys):
+        harms = []
+        if pc_set in a:
+            harms.extend(a[pc_set])
+        if pc_set in b:
+            harms.extend(b[pc_set])
+        c[pc_set] = harms
+    return c
+
 
 if __name__ == '__main__':
     logger = initLogger()
     import chord
-    major = chord.base_major
-    major_harm = major.keys()
-    for key in major_keys:
-        for inversion in ['', 'b', 'c', 'd']:
-            for harm in major_harm:
-                chordstr = '{}:{}{}'.format(key, harm, inversion)
-                if inversion == 'd':
-                    if harm != 'Gn' and harm != 'Fr' and not '7' in harm:
-                        continue
-                c = chord.Chord(chordstr)
-                logger.info('{} | {} {} | {} | inversion {} | bass {} | {}'.format(c.harmstr, c.key, 'minor' if c.key.islower() else 'major', c.harm, c.inversion, c.bass, c.pitch_classes))
-                pc_set = major_pc_sets.get(c.pitch_classes, [])
-                pc_set.append(c.harmstr)
-                major_pc_sets[c.pitch_classes] = pc_set
-    pp.pprint(major_pc_sets)
+    major_pc_sets = get_pc_sets(chord.base_major, major_keys)
+    minor_pc_sets = get_pc_sets(chord.base_minor, minor_keys)
+    all_pc_sets = add_pc_sets(major_pc_sets, minor_pc_sets)
+
+
 
 
 
