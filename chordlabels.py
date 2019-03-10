05be918961c53f7158ff1cd25c0938c3b8ca6527
Nestor Napoles <napulen@gmail.com>
2019-03-09 21:10:36 -0500

    New: Adding provisional labeling of the second layer of the model

---
+++ chordlabels.py
@@ -0,0 +1,136 @@
+
+chord_labels = {
+    frozenset({8, 3, 11, 5}): 'Fhdim7',
+    frozenset({9, 2, 6}): 'DMaj',
+    frozenset({1, 10, 6}): 'GbMaj',
+    frozenset({9, 2, 11, 6}): 'Bhdim7',
+    frozenset({9, 3, 5}): 'F7_no5',
+    frozenset({8, 2, 4}): 'E7_no5',
+    frozenset({1, 10, 5, 6}): 'F#M7',
+    frozenset({10, 2, 11, 6}): 'BminM7',
+    frozenset({10, 4, 7}): 'Edim',
+    frozenset({1, 3, 7}): 'Eb7_no5',
+    frozenset({9, 2, 5}): 'Dmin',
+    frozenset({0, 9, 6}): 'F#dim',
+    frozenset({11, 5, 7}): 'G7_no5',
+    frozenset({8, 0, 3, 6}): 'Ab7',
+    frozenset({8, 0, 3, 7}): 'AbM7',
+    frozenset({3, 9, 11, 6}): 'B7',
+    frozenset({8, 2, 11, 5}): 'Ddim7',
+    frozenset({10, 2, 5}): 'BbMaj',
+    frozenset({8, 1, 10, 5}): 'Bb7',
+    frozenset({9, 2, 6, 1}): 'DM7',
+    frozenset({8, 0, 4}): 'CAug',
+    frozenset({0, 10, 4}): 'C7_no5',
+    frozenset({9, 5, 6, 1}): 'F#minM7',
+    frozenset({10, 2, 5, 6}): 'GbAugM7',
+    frozenset({3, 9, 11}): 'B7_no5',
+    frozenset({0, 11, 4, 7}): 'CM7',
+    frozenset({9, 4, 5, 1}): ''
+    frozenset({11, 4, 7}):
+    frozenset({0, 10, 4, 6}):
+    frozenset({8, 1, 10, 4}):
+    frozenset({8, 0, 4, 7}):
+    frozenset({1, 10, 7}):
+    frozenset({2, 10, 3, 7}):
+    frozenset({1, 11, 5, 7}):
+    frozenset({8, 9, 5, 1}):
+    frozenset({8, 10, 2, 5}):
+    frozenset({1, 2, 10, 6}):
+    frozenset({1, 10, 5}):
+    frozenset({1, 10, 5, 9}):
+    frozenset({8, 1, 11, 5}):
+    frozenset({0, 9, 2, 5}):
+    frozenset({11, 3, 7}):
+    frozenset({0, 10, 3, 6}):
+    frozenset({8, 10, 2}):
+    frozenset({0, 10, 3, 7}):
+    frozenset({9, 5, 1}):
+    frozenset({1, 10, 4, 6}):
+    frozenset({8, 2, 5}):
+    frozenset({10, 2, 4, 7}):
+    frozenset({9, 4, 1, 7}):
+    frozenset({0, 9, 4, 8}):
+    frozenset({9, 3, 6, 1}):
+    frozenset({2, 10, 3, 6}):
+    frozenset({10, 4, 6}):
+    frozenset({0, 9, 4, 7}):
+    frozenset({0, 9, 2, 6}):
+    frozenset({8, 0, 3, 5}):
+    frozenset({9, 4, 1}):
+    frozenset({10, 2, 6}):
+    frozenset({9, 6, 1}):
+    frozenset({8, 0, 3}):
+    frozenset({3, 11, 6, 7}):
+    frozenset({2, 11, 7}):
+    frozenset({9, 3, 1, 7}):
+    frozenset({10, 2, 5, 7}):
+    frozenset({0, 9, 3, 5}):
+    frozenset({8, 2, 11}):
+    frozenset({0, 11, 3, 7}):
+    frozenset({3, 10, 11, 6}):
+    frozenset({8, 1, 5}):
+    frozenset({2, 11, 5}):
+    frozenset({2, 11, 4, 7}):
+    frozenset({8, 1, 5, 0}):
+    frozenset({3, 11, 6}):
+    frozenset({9, 3, 6}):
+    frozenset({9, 10, 2, 5}):
+    frozenset({1, 4, 7}):
+    frozenset({0, 9, 4, 5}):
+    frozenset({0, 3, 6}):
+    frozenset({1, 11, 4, 7}):
+    frozenset({3, 10, 11, 7}):
+    frozenset({0, 8, 11, 4}):
+    frozenset({8, 1, 11, 4}):
+    frozenset({2, 11, 6}):
+    frozenset({0, 9, 5}):
+    frozenset({9, 1, 7}):
+    frozenset({11, 2, 3, 7}):
+    frozenset({1, 10, 5, 7}):
+    frozenset({8, 2, 11, 4}):
+    frozenset({8, 3, 11}):
+    frozenset({9, 2, 5, 1}):
+    frozenset({9, 10, 2, 6}):
+    frozenset({8, 2, 11, 6}):
+    frozenset({8, 0, 5}):
+    frozenset({8, 0, 2, 6}):
+    frozenset({10, 3, 6}):
+    frozenset({0, 3, 7}):
+    frozenset({9, 2, 11, 5}):
+    frozenset({8, 0, 4, 5}):
+    frozenset({1, 10, 4, 7}):
+    frozenset({0, 9, 4}):
+    frozenset({0, 1, 5, 9}):
+    frozenset({10, 2, 6, 7}):
+    frozenset({8, 1, 4}):
+    frozenset({8, 9, 4, 1}):
+    frozenset({1, 10, 4}):
+    frozenset({0, 4, 7}):
+    frozenset({0, 9, 3}):
+    frozenset({0, 2, 6}):
+    frozenset({10, 2, 7}):
+    frozenset({2, 11, 5, 7}):
+    frozenset({8, 1, 4, 0}):
+    frozenset({1, 10, 3, 7}):
+    frozenset({1, 10, 3, 6}):
+    frozenset({3, 9, 11, 5}):
+    frozenset({8, 11, 5}):
+    frozenset({0, 10, 4, 7}):
+    frozenset({0, 9, 4, 6}):
+    frozenset({3, 11, 4, 7}):
+    frozenset({8, 3, 11, 6}):
+    frozenset({0, 9, 3, 7}):
+    frozenset({9, 4, 6, 1}):
+    frozenset({8, 3, 11, 4}):
+    frozenset({8, 0, 3, 4}):
+    frozenset({2, 11, 6, 7}):
+    frozenset({8, 3, 11, 7}):
+    frozenset({8, 11, 4}):
+    frozenset({1, 11, 5}):
+    frozenset({8, 0, 2, 5}):
+    frozenset({8, 0, 6}):
+    frozenset({0, 9, 3, 6}):
+    frozenset({10, 3, 7}):
+    frozenset({8, 10, 2, 4}):
+}
